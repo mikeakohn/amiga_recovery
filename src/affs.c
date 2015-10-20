fd0ab15703f471b0ff83bd0ebe88219801cbacde
Michael A. Kohn <mike@mikekohn.net>
2015-10-20 12:46:12 -0500

    Separating directory code from affs.c.

--- src/affs.c
+++ src/affs.c
@@ -24,10 +24,10 @@ Released under GPL
 #define ST_LINKDIR 4 
 
 /* hash_name function copied from http://lclevy.free.fr/adflib/adf_info.html */
-static unsigned int hash_name(unsigned char *name)
+uint32_t hash_name(unsigned char *name)
 {
-uint32_t hash, l; /* sizeof(int)>=2 */
-int i;
+  uint32_t hash, l; /* sizeof(int)>=2 */
+  int i;
 
   l = hash = strlen((char *)name);
 
@@ -70,60 +70,6 @@ int t;
   file_ext->sec_type = read_int(in);
 }
 
-void read_directory(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_directory *directory, unsigned int block)
-{
-int namelen;
-int t;
-
-  fseek(in, partition->start + (block * bootblock->blksz), SEEK_SET);
-
-  directory->type = read_int(in);
-  directory->header_key = read_int(in);
-  //unsigned int unused1[3];
-  fseek(in, 3 * 4, SEEK_CUR);
-  directory->checksum = read_int(in);
-  //unsigned int hash_table[512/4-56];
-  for (t = 0; t < (512 / 4 - 56); t++)
-  {
-    directory->hash_table[t] = read_int(in);
-  }
-  //unsigned int unused2[2];
-  fseek(in, 2 * 4, SEEK_CUR);
-  directory->uid = read_short(in);
-  directory->gid = read_short(in);
-  directory->protect = read_int(in);
-  // FIXME - WTF?
-  //directory->unused3 = read_int(in);
-  //unsigned char comment[80]; // first char is len
-  namelen = getc(in);
-  for (t = 0; t < 79; t++)
-  {
-    directory->comment[t] = getc(in);
-  }
-  directory->comment[namelen]  =0;
-  //unsigned char unused4[12];
-  fseek(in, 12, SEEK_CUR);
-  directory->days= read_int(in);
-  directory->mins = read_int(in);
-  directory->ticks = read_int(in);
-  //unsigned char dirname[32]; // first char is len, last char is unused
-  namelen = getc(in);
-  for (t = 0; t < 31; t++)
-  {
-    directory->dirname[t] = getc(in);
-  }
-  directory->dirname[namelen] = 0;
-  //unsigned int unused5[2];
-  fseek(in, 2 * 4, SEEK_CUR);
-  directory->next_link = read_int(in);
-  //unsigned int unused6[5];
-  fseek(in, 5 * 4, SEEK_CUR);
-  directory->hash_chain = read_int(in);
-  directory->parent = read_int(in);
-  directory->extension = read_int(in);
-  directory->sec_type = read_int(in);
-}
-
 void read_datablock(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_datablock *datablock, unsigned int block)
 {
 int t;
@@ -142,7 +88,7 @@ int t;
   }
 }
 
-static void print_hash_info(FILE *in, struct _amiga_rootblock *rootblock, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, unsigned int block)
+void print_hash_info(FILE *in, struct _amiga_rootblock *rootblock, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, uint32_t block)
 {
 struct _amiga_directory directory;
 struct _amiga_fileheader fileheader;
@@ -176,22 +122,6 @@ int sec_type;
     else
   {
     printf("Unknown sec_type %d\n", sec_type);
-  }
-}
-
-void list_directory(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd)
-{
-//struct _amiga_rootblock rootblock;
-int t;
-
-  //read_rootblock(in,bootblock,partition,&rootblock);
-
-  for (t = 0; t < 72; t++)
-  {
-    if (pwd->dir_hash[t] != 0)
-    {
-      print_hash_info(in, &pwd->rootblock, bootblock, &pwd->partition, pwd->dir_hash[t]);
-    }
   }
 }
 
@@ -324,36 +254,6 @@ int t;
   printf("         sec_type: %d\n", file_ext->sec_type);
 }
 
-void print_directory(struct _amiga_directory *directory)
-{
-int t;
-
-  printf("================== Directory ===================\n");
-  printf("             type: %d\n", directory->type);
-  printf("       header_key: %d\n", directory->header_key);
-  printf("         checksum: %d\n", directory->checksum);
-  printf("       hash_table:");
-  for (t = 0; t < (512 / 4 - 56); t++)
-  {
-    if (t != 0) { printf(" "); }
-    printf("%d", directory->hash_table[t]);
-  }
-  printf("\n");
-  printf("              uid: %d\n", directory->uid);
-  printf("              gid: %d\n", directory->gid);
-  printf("          protect: %d\n", directory->protect);
-  printf("          comment: %s\n", directory->comment);
-  printf("             days: %d\n", directory->days);
-  printf("             mins: %d\n", directory->mins);
-  printf("            ticks: %d\n", directory->ticks);
-  printf("          dirname: %s\n", directory->dirname);
-  printf("        next_link: %d\n", directory->next_link);
-  printf("       hash_chain: %d\n", directory->hash_chain);
-  printf("           parent: %d\n", directory->parent);
-  printf("        extension: %d\n", directory->extension);
-  printf("         sec_type: %d\n", directory->sec_type);
-}
-
 void print_datablock(struct _amiga_datablock *datablock)
 {
 char ascii[17];
@@ -394,85 +294,10 @@ int t;
   //FIXME - stuff missing at bottom..
 }
 
-int ch_dir(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd, char *dirname)
-{
-struct _amiga_directory directory;
-struct _amiga_fileheader fileheader;
-
-  if (strcmp(dirname, "..")==0)
-  {
-    if (pwd->parent_dir == 0) { return -1; }
-
-    if (pwd->parent_dir==pwd->rootblock.header_key)
-    {
-      memcpy(pwd->dir_hash, pwd->rootblock.hash_table, (BSIZE / 4 - 56) * 4);
-      pwd->cwd[0] = 0;
-      pwd->parent_dir = 0;
-    }
-      else
-    {
-      read_directory(in, bootblock, &pwd->partition, &directory, pwd->parent_dir);
-      memcpy(pwd->dir_hash, directory.hash_table, (BSIZE / 4 - 56) * 4);
-      int l = strlen(pwd->cwd) - 2;
-      while(l >= 0)
-      {
-        if (pwd->cwd[l] == '/') break;
-        pwd->cwd[l--] = 0;
-      }
-    }
-  }
-    else
-  {
-    int block = pwd->dir_hash[hash_name((unsigned char*)dirname)];
-
-    while(block != 0)
-    {
-      unsigned int sec_type = get_sec_type(in,bootblock,&pwd->partition,block);
-
-      if (sec_type == ST_USERDIR)
-      {
-        read_directory(in,bootblock,&pwd->partition,&directory,block);
-
-        if (strcmp((char *)directory.dirname, dirname)==0)
-        {
-          memcpy(pwd->dir_hash, directory.hash_table, (BSIZE / 4 - 56) * 4);
-          pwd->parent_dir = directory.parent;
-          strcat(pwd->cwd, dirname);
-          strcat(pwd->cwd, "/");
-          return 0;
-        }
-
-        if (directory.hash_chain == 0) break;
-        block = directory.hash_chain;
-      }
-        else
-      if (sec_type == ST_FILE)
-      {
-        read_fileheader(in, bootblock, &pwd->partition, &fileheader, block);
-
-        if (fileheader.hash_chain == 0) break;
-
-        block = fileheader.hash_chain;
-      }
-        else
-      {
-        printf("Unknown sec_type %d\n", sec_type);
-        break;
-      }
-    }
-
-    printf("Error: No such directory\n");
-    return -1;
-  }
-
-  return 0;
-}
-
 int get_sec_type(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, unsigned int block)
 {
   fseek(in, partition->start + (block * bootblock->blksz + (bootblock->blksz - 4)), SEEK_SET);
 
   return read_int(in);
 }
-
 
--- src/affs.h
+++ src/affs.h
@@ -12,6 +12,7 @@ Released under GPL
 #define AFFS_H
 
 #include "bootblock.h"
+#include "directory.h"
 #include "fileheader.h"
 #include "partition.h"
 #include "rootblock.h"
@@ -32,33 +33,6 @@ struct _amiga_file_ext
   unsigned int sec_type;
 };
 
-struct _amiga_directory
-{
-  unsigned int type;
-  unsigned int header_key;
-  unsigned int unused1[3];
-  unsigned int checksum;
-  unsigned int hash_table[BSIZE/4-56];
-  unsigned int unused2[2];
-  unsigned short int uid;
-  unsigned short int gid;
-  unsigned int protect;
-  unsigned int unused3;
-  unsigned char comment[80]; // first char is len
-  unsigned char unused4[12];
-  unsigned int days;
-  unsigned int mins;
-  unsigned int ticks;
-  unsigned char dirname[32]; // first char is len, last char is unused
-  unsigned int unused5[2];
-  unsigned int next_link;
-  unsigned int unused6[5];
-  unsigned int hash_chain;
-  unsigned int parent;
-  unsigned int extension;
-  unsigned int sec_type;
-};
-
 struct _amiga_datablock
 {
   unsigned int type;
@@ -70,30 +44,17 @@ struct _amiga_datablock
   unsigned char data[BSIZE-24];
 };
 
-// I didn't want to put this here, but owell
-struct _pwd
-{
-  struct _amiga_rootblock rootblock;
-  struct _amiga_partition partition;
-  int partition_num;
-  char cwd[4096];  // FIXME - check bounds
-  unsigned int dir_hash[BSIZE/4-56];
-  unsigned int hash_table_size;
-  unsigned int parent_dir;
-};
-
 void read_file_ext(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_file_ext *file_ext, unsigned int block);
-void read_directory(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_directory *directory, unsigned int block);
 void read_datablock(FILE *in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_datablock *datablock, unsigned int block);
 
-void list_directory(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd);
 void print_file(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd, char *filename, FILE *out);
 
 void print_file_ext(struct _amiga_file_ext *file_ext);
-void print_directory(struct _amiga_directory *directory);
 void print_datablock(struct _amiga_datablock *datablock);
 
-int ch_dir(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd, char *dirname);
+uint32_t hash_name(unsigned char *name);
+void print_hash_info(FILE *in, struct _amiga_rootblock *rootblock, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, uint32_t block);
+
 int get_sec_type(FILE *in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, unsigned int block);
 
 #endif
