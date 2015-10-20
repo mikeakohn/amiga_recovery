728af0836bcc3c691d910a09765c690ac35c3a22
Michael A. Kohn <mike@mikekohn.net>
2015-10-20 12:49:34 -0500

    Changing unsigned int to uint32_t.

--- src/directory.h
+++ src/directory.h
@@ -18,29 +18,29 @@ Released under GPL
 
 struct _amiga_directory
 {
-  unsigned int type;
-  unsigned int header_key;
-  unsigned int unused1[3];
-  unsigned int checksum;
-  unsigned int hash_table[BSIZE/4-56];
-  unsigned int unused2[2];
-  unsigned short int uid;
-  unsigned short int gid;
-  unsigned int protect;
-  unsigned int unused3;
+  uint32_t type;
+  uint32_t header_key;
+  uint32_t unused1[3];
+  uint32_t checksum;
+  uint32_t hash_table[BSIZE/4-56];
+  uint32_t unused2[2];
+  uint16_t uid;
+  uint16_t gid;
+  uint32_t protect;
+  uint32_t unused3;
   unsigned char comment[80]; // first char is len
   unsigned char unused4[12];
-  unsigned int days;
-  unsigned int mins;
-  unsigned int ticks;
+  uint32_t days;
+  uint32_t mins;
+  uint32_t ticks;
   unsigned char dirname[32]; // first char is len, last char is unused
-  unsigned int unused5[2];
-  unsigned int next_link;
-  unsigned int unused6[5];
-  unsigned int hash_chain;
-  unsigned int parent;
-  unsigned int extension;
-  unsigned int sec_type;
+  uint32_t unused5[2];
+  uint32_t next_link;
+  uint32_t unused6[5];
+  uint32_t hash_chain;
+  uint32_t parent;
+  uint32_t extension;
+  uint32_t sec_type;
 };
 
 // I didn't want to put this here, but owell
@@ -50,12 +50,12 @@ struct _pwd
   struct _amiga_partition partition;
   int partition_num;
   char cwd[4096];  // FIXME - check bounds
-  unsigned int dir_hash[BSIZE/4-56];
-  unsigned int hash_table_size;
-  unsigned int parent_dir;
+  uint32_t dir_hash[BSIZE/4-56];
+  uint32_t hash_table_size;
+  uint32_t parent_dir;
 };
 
-void read_directory(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_directory *directory, unsigned int block);
+void read_directory(FILE * in, struct _amiga_bootblock *bootblock, struct _amiga_partition *partition, struct _amiga_directory *directory, uint32_t block);
 void list_directory(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd);
 void print_directory(struct _amiga_directory *directory);
 int ch_dir(FILE *in, struct _amiga_bootblock *bootblock, struct _pwd *pwd, char *dirname);
